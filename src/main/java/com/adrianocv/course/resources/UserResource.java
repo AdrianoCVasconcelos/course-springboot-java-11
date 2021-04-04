4502efe6e497948755a6749deae7a4ba03a97e02
AdrianoCVasconcelos <adriano.cvasconcelos@hotmail.com>
2021-04-04 10:42:04 -0300

    User entitiy and resource

---
+++ src/main/java/com/adrianocv/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.adrianocv.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adrianocv.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","maria@gamil.com","8599869951","11234567");
+		return ResponseEntity.ok().body(u);
+	
+	}
+
+}
