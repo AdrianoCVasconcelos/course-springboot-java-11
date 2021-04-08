5b9f07485a32735855a9ce8fb8dc2442f5cbf29f
AdrianoCVasconcelos <adriano.cvasconcelos@hotmail.com>
2021-04-08 18:19:58 -0300

    Payment one-one association

--- src/main/java/com/adrianocv/course/config/TesteConfig.java
+++ src/main/java/com/adrianocv/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.adrianocv.course.entities.Category;
 import com.adrianocv.course.entities.Order;
 import com.adrianocv.course.entities.OrderItem;
+import com.adrianocv.course.entities.Payment;
 import com.adrianocv.course.entities.Product;
 import com.adrianocv.course.entities.User;
 import com.adrianocv.course.entities.enums.OrderStatus;
@@ -83,5 +84,9 @@ public class TesteConfig implements CommandLineRunner{
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);		
 	}	
 }
